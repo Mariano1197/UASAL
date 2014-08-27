ec8ded7d95c1f7dbac3bc062ec2a4fb2a2ce2448
matiast02 <matix_tkd@hotmail.com>
2014-08-27 00:50:42 -0300

    Create header.tempalte.php
    Cabecera de las paginas modulada para ser reutilizada.

---
+++ header.tempalte.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Gestion</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='css/reset.css' type='text/css' rel='stylesheet' />
+	<link href='css/bootstrap.css' type='text/css' rel='stylesheet' />
+	<link href='css/default.css' type='text/css' rel='stylesheet' />
+	<script src='js/jquery-1.11.1.min.js' type="text/javascript"></script>
+	<script src='js/scripts.js' type="text/javascript"></script>
+	<script src='js/bootstrap.min.js' type="text/javascript"></script>
+	<script src='js/bootbox.min.js' type="text/javascript"></script>
+	
+</head>
